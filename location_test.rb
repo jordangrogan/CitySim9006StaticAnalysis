8c89f69059a98a593a0bba8c8ad671cef9af7b4a
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-03 18:51:23 -0400

    Removed unit tests for old cold

--- location_test.rb
+++ location_test.rb
@@ -13,38 +13,6 @@ class LocationTest < Minitest::Test
     @l = Location::new("Test Location")
   end
 
-  # UNIT TESTS FOR METHOD add_roads(*roads)
-  # Equivalence classes:
-  # Number of roads < 1 -> return nil
-  # Number of roads = 1 -> add it to @roads
-  # Number of roads > 1 -> add them to @roads
-
-  # If the number of roads < 1, return nil
-  # EDGE CASE
-  # def test_add_roads_less_than_one
-  #   assert_nil @l.add_roads()
-  # end
-  #
-  # # If the number of roads being added = 1, add it to the @roads array.
-  # # This test ensures that the new road is included in the @roads array (by making a road double).
-  # # BASE CASE
-  # def test_add_roads_one
-  #   road1 = Minitest::Mock.new("test_road_1")
-  #   @l.add_roads(road1)
-  #   assert_includes @l.roads, road1
-  # end
-  #
-  # # If the number of roads being added > 1, add the roads to the @roads array.
-  # # This test ensures that the new roads are included in the @roads array (by making 2 road doubles).
-  # # BASE CASE
-  # def test_add_roads_more_than_one
-  #   road1 = Minitest::Mock.new("test_road_1")
-  #   road2 = Minitest::Mock.new("test_road_2")
-  #   @l.add_roads(road1, road2)
-  #   assert_includes @l.roads, road1
-  #   assert_includes @l.roads, road2
-  # end
-
   # UNIT TESTS FOR METHOD get_next_road(prng)
   # The parameter is a pseudorandom number generator, so it doesn't make sense for their to be equivalence classes
   # The pseudorandom number generator's rand function in this unit test is stubbed
